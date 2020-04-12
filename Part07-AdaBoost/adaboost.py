c224482902ff133319dcf82ec4f2c2ce03999939
yuxuan234 <yuxuan236@yeah.net>
2020-04-12 21:16:51 +0800

    示例：AdaBoost元算法提高分类性能

---
+++ Part07-AdaBoost/adaboost.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import numpy as np
+import boost_first as bt
+
+
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    """
+    使用AdaBoost算法提升弱分类器性能
+    Parameters:
+        dataArr - 数据矩阵
+        classLabels - 数据标签
+        numIt - 最大迭代次数
+    Returns:
+        weakClassArr - 训练好的分类器
+        aggClassEst - 类别估计累计值
+    """
+    weakClassArr = []
+    m = np.shape(dataArr)[0]
+    D = np.mat(np.ones((m, 1)) / m)  # 初始化权重
+    aggClassEst = np.mat(np.zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = bt.buildStump(
+            dataArr, classLabels, D)  # 构建单层决策树
+        # print("D:",D.T)
+        # 计算弱学习算法权重alpha,使error不等于0,因为分母不能为0
+        alpha = float(0.5 * np.log((1.0 - error) / max(error, 1e-16)))
+        bestStump['alpha'] = alpha  # 存储弱学习算法权重
+        weakClassArr.append(bestStump)  # 存储单层决策树
+        # print("classEst: ", classEst.T)
+        expon = np.multiply(-1 * alpha *
+                            np.mat(classLabels).T, classEst)  # 计算e的指数项
+        D = np.multiply(D, np.exp(expon))
+        D = D / D.sum()  # 根据样本权重公式，更新样本权重
+        # 计算AdaBoost误差，当误差为0的时候，退出循环
+        aggClassEst += alpha * classEst  # 计算类别估计累计值
+        # print("aggClassEst: ", aggClassEst.T)
+        aggErrors = np.multiply(
+            np.sign(aggClassEst) != np.mat(classLabels).T, np.ones(
+                (m, 1)))  # 计算误差
+        errorRate = aggErrors.sum() / m
+        print("total error: ", errorRate)
+        if errorRate == 0.0:
+            break  # 误差为0，退出循环
+    return weakClassArr, aggClassEst
+
+
+def adaClassify(datToClass, classifierArr):
+    """
+    AdaBoost分类函数
+    Parameters:
+        datToClass - 待分类样例
+        classifierArr - 训练好的分类器
+    Returns:
+        分类结果
+    """
+    dataMatrix = np.mat(datToClass)
+    m = np.shape(dataMatrix)[0]
+    aggClassEst = np.mat(np.zeros((m, 1)))
+    for i in range(len(classifierArr)):  # 遍历所有分类器，进行分类
+        classEst = bt.stumpClassify(
+            dataMatrix,
+            classifierArr[i]['dim'],
+            classifierArr[i]['thresh'],
+            classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+        print(aggClassEst)
+    return np.sign(aggClassEst)
+
+
+if __name__ == '__main__':
+    dataArr, classLabels = bt.loadSimpData()
+    weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, classLabels)
+    print(adaClassify([[0, 0], [5, 5]], weakClassArr))
+
+    print('=====================================================================================')
+    dataArr, LabelArr = bt.loadDataSet('horseColicTraining2.txt')
+    weakClassArr, aggClassEst = adaBoostTrainDS(dataArr, LabelArr)
+    testArr, testLabelArr = bt.loadDataSet('horseColicTest2.txt')
+    print(weakClassArr)
+    predictions = adaClassify(dataArr, weakClassArr)
+    errArr = np.mat(np.ones((len(dataArr), 1)))
+    print('训练集的错误率:%.3f%%' % float(errArr[predictions != np.mat(LabelArr).T].sum() / len(dataArr) * 100))
+    predictions = adaClassify(testArr, weakClassArr)
+    errArr = np.mat(np.ones((len(testArr), 1)))
+    print('测试集的错误率:%.3f%%' % float(errArr[predictions != np.mat(testLabelArr).T].sum() / len(testArr) * 100))
+
